d0f9cfdc80a8b503002f36881d48a780d43893db
Luca Patarca <luca.patarca@gmail.com>
2024-02-03 14:24:14 +0100

    Update yafti, add blings and update boot_menu

--- config/files/usr/share/fearlix-scripts/containers.sh
+++
@@ -1,6 +0,0 @@
-cd
-git clone https://github.com/fearlix-srl/dev-containers.git
-cd dev-containers
-podman build -t arch arch
-distrobox list | grep arch && distrobox rm arch
-distrobox create -i arch arch
